902c2951e7ca24778af6e4006629b15571ed12a7
jsw <954951185@qq.com>
2026-04-10 23:47:21 +0800

    更新章节文件路径，修改语音类型和语音速度设置，添加完成打印信息

--- main.py
+++ main.py
@@ -143,18 +143,18 @@ if __name__ == "__main__":
     #     if os.path.exists(output_path):
     #         os.remove(output_path)
     #     main(file_path, output_path)
-    # txt = "第0010章-贪腐枉法，日讲太甲"
-    txt = rf"txt\第0092章-阴风晦冥，恶贯祸盈.txt"
+    # txt = rf"txt\第0119章-吟诗作赋，褰裳躩步.txt"
+    txt = rf"txt\第0122章-后继之人，连昬接晨.txt"
     file_path = rf"D:\做视频\大号B站\136-万历明君\{txt}"
-    # file_path = rf"C:\Users\jsw\Documents\00project\wen_an\0020-清朝政府是中国人的政府吗？.txt"
-    # file_path = rf"C:\Users\jsw\Documents\00project\wen_an\团结壬与LGBT.txt"
     voice_type = "zh-CN-YunjianNeural"
     file_name = os.path.basename(file_path).replace(".txt", "")
     output_path = fr"D:\做视频\大号B站\136-万历明君\mp3\{file_name}-{voice_type}.mp3"
-    # output_path = fr"{file_name}-{voice_type}.mp3"
 
     speed = "+55%"
     # 删除
     if os.path.exists(output_path):
         os.remove(output_path)
     main(file_path, output_path, speed=speed)
+
+    # 完成 打印文件名 file_path
+    print(f"完成：{file_name}")
